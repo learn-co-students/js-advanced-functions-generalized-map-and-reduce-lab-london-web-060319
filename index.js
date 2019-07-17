c6a843cf428166367dc8c376c98801a611b608ad
github username <github email address>
2019-07-17 18:55:44 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your functions here
+
+const map = (array, block) => {
+    const newArray = new Array
+    array.forEach( a => {newArray.push(block(a))} )
+    return newArray
+}
+
+const reduce = (array, block, starting) => {
+    let startPoint,
+        index 
+        
+    startPoint = !!starting ? starting : array[0]
+    index = !!starting ? 0 : 1
+
+    while (index<array.length)  {
+        startPoint = block(array[index], startPoint)
+        index++
+    }
+    return startPoint
+}
+
