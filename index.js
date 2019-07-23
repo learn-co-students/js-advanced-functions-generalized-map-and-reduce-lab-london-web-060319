e84cbdd51850fda4662a456235414503eb4656d8
Danielle <dmiz@brandeis.edu>
2019-07-23 14:10:52 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your functions here
+
+function map(src,fctn){
+    let n=[]
+
+    for (let i=0; i<src.length;i++){
+        n.push(fctn(src[i]))
+    }
+    return n;
+}
+
+function reduce(src,fctn,start){
+    let r = (!!start) ? start :src[0]
+    let i= (!!start) ? 0:1
+    
+    for (; i<src.length; i++){
+        r= fctn(src[i], r)
+    }
+    return r 
+}
