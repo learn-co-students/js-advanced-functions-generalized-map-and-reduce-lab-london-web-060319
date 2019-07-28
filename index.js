e42bfa33ab0c4c6976f2eb96c31dbbd6e4c50a88
Louise Norris <louisenorris73@yahoo.co.uk>
2019-07-28 14:19:56 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Add your functions here
+function map(sourceArray, callBackFcn) {
+  let negArr = []
+  for (let i = 0; i < sourceArray.length; i++ ) {
+    let el = sourceArray[i]
+    negArr.push(callBackFcn(el))
+  }
+  return negArr
+}
+
+function reduce(sourceArray, callBackFcn, starting) {
+  let arr = (!!starting) ? starting : sourceArray[0]
+  let i = (!!starting) ? 0 : 1
+  for (; i < sourceArray.length; i++) {
+    arr = callBackFcn(sourceArray[i], arr)
+  }
+  return arr;
+}
